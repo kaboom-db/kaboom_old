89acec1c781f5cb19848bd48af24a155314c64e5
Tanveer Najib <tanveernajib@gmail.com>
2022-08-18 12:14:18 +0100

    initial commit

---
+++ web/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    # path('comics/', include('comics.urls'))
+]
