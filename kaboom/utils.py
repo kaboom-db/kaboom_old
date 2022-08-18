ac2097da80942c8bf765dba2da6f238fa4d4595a
Tanveer Najib <tanveernajib@gmail.com>
2022-08-18 10:29:37 +0100

    fresh reset of kaboom

--- kaboom/utils.py
+++
@@ -1,9 +0,0 @@
-from libgravatar import Gravatar
-
-def get_user_image(email) -> str:
-    g = Gravatar(email)
-    image = g.get_image(default='retro')
-    return image
-
-def util_calculate_age(dob, dod) -> int:
-    return dod.year - dob.year - ((dod.month, dod.day) < (dob.month, dob.day))
